b8899075d78981335495296bd1443b076ed401f1
Ekansh10 <ekanshmahajan1010@gmail.com>
2025-08-26 10:27:13 +0530

    TopoSort using BFS | Kahn's Algo

--- TopologicalSort.java
+++ TopologicalSort.java
@@ -30,6 +30,40 @@ public class TopologicalSort {
         }
         return res;
     }
+
+    static int[] topoSortBFS(ArrayList<ArrayList<Integer>> adjlst, int V){ // Also known as Kahn's Algorithm
+        int[] indegree = new int[V];
+        for(int i = 0; i<V; i++){
+            indegree[i] = 0;
+        }
+        for(ArrayList<Integer> adj : adjlst){
+            for(int i : adj){
+                indegree[i]++;
+            }
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+        for(int i = 0; i<V; i++){
+            if(indegree[i] == 0){
+                q.add(i);
+            }
+        }
+
+        int[] res = new int[V];
+        int idx = 0;
+        while(!q.isEmpty()){
+            int n = q.poll();
+            ArrayList<Integer> arr = adjlst.get(n);
+            for(int neigh : arr){
+                indegree[neigh]--;
+                if(indegree[neigh] == 0) q.add(neigh);
+            }
+            res[idx] = n;
+            idx++;
+        }
+        return res;
+    }
+
     public static void main(String[] args) {
         ArrayList<Integer> v0 = new ArrayList<>();
         ArrayList<Integer> v1 = new ArrayList<>();
@@ -53,9 +87,16 @@ public class TopologicalSort {
         adjlst.add(v5);
 
         int[] result = topoSortDFS(adjlst, 6);
+        int[] result1= topoSortBFS(adjlst, 6);
 
-        System.out.println("Topological Sort: ");
+        System.out.println("Topological Sort for DFS: ");
         for(int i : result){
+            System.out.print(i+" ");
+        }
+        System.out.println("");
+
+        System.out.println("Topological Sort for BFS: ");
+        for(int i : result1){
             System.out.print(i+" ");
         }
         System.out.println("");
